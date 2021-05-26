970f707af7b80e16a63f7989ec63c84af99e5f1a
Caleb Krueger <>
2021-05-26 03:23:41 +0000

    updated part e

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ console.log(cargoHold);
 
 //c) Query the user to select a cabinet (0 - 3) in the cargoHold.
 const input = require('readline-sync');
-let userSelection = input.question("Select a cabinet (0 - 3) in the cargoHold:");
+/*let userSelection = input.question("Select a cabinet (0 - 3) in the cargoHold:");*/
 
 
 //d) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-if (userSelection >= 0 && userSelection <= 3){
+/*if (userSelection >= 0 && userSelection <= 3){
   console.log(cargoHold[userSelection]);
-}else console.log('Invalid entry');
+}else console.log('Invalid entry');*/
 
 
 //e) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
-/*let userSelection2 = input.question("Select a cabinet (0 - 3) in the cargoHold:");
+let userSelection2 = input.question("Select a cabinet (0 - 3) in the cargoHold:");
 let userSelection3 = input.question("Enter item name:");
-if (userSelection2 >= 0 && userSelection <= 3 && cargoHold[userSelection2].includes(userSelection3.toLowerCase())){ 
+if (userSelection2 >= 0 && userSelection2 <= 3 && cargoHold[userSelection2].includes(userSelection3.toLowerCase())){ 
   console.log(`Cabinet ${userSelection2} DOES contain ${userSelection3}.`);
 } else
-console.log(`Cabinet ${userSelection2} DOES NOT contain ${userSelection3}.`);*/
+console.log(`Cabinet ${userSelection2} DOES NOT contain ${userSelection3}.`);
